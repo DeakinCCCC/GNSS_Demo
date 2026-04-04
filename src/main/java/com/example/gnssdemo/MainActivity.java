ac153e8631bbc774474c7fd79155dd96058552f7
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2026-04-05 03:38:32 +0800

    Add files via upload

---
+++ src/main/java/com/example/gnssdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.gnssdemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.chaquo.python.PyObject;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+		if (!Python.isStarted()) {
+			Python.start(new AndroidPlatform(this));
+		}
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+				Python py = Python.getInstance();
+				PyObject module = py.getModule("rtk_demo");
+				PyObject result = module.callAttr("rtk_test", 5, 3);
+				int sum = result.toInt();
+                Toast.makeText(MainActivity.this, "Hello, Android! Result is "+sum, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ src/main/python/rtk_demo.py
@@ -0,0 +1,4 @@
+def rtk_test(a, b):
+    stream = rtklib.Stream()
+    stream.close()
+    return a + b
