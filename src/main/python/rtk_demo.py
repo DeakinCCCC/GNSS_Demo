43b4b56b62c25bdbc642bfc6f746c9760ee8da53
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2026-04-05 12:47:25 +0800

    Update rtk_demo.py

--- src/main/python/rtk_demo.py
+++ src/main/python/rtk_demo.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 def rtk_test(a, b):
-    stream = rtklib.Stream()
-    stream.close()
-    return a + b
+    #stream = rtklib.Stream()
+    #stream.close()
+    arr = np.array([1, 2, 3, 4, 5])
+    return a + b + arr[0]
