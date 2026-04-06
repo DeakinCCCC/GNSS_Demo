094cd3c6262e8d3c98a87c90c6b2386b4c91ad50
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2026-04-07 00:47:31 +0800

    Update MainActivity.java

--- src/main/java/com/example/gnssdemo/MainActivity.java
+++ src/main/java/com/example/gnssdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gnssdemo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
